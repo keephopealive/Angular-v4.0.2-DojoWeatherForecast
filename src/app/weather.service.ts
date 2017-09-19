a5546a9cc94542fb4773261cb815419945cb8fb0
Speros Misirlakis <speros.misirlakis@gmail.com>
2017-09-19 16:04:05 -0700

    Update weather.service.ts

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -8,7 +8,7 @@ export class WeatherService {
   constructor(private _http: Http) { }
 
   getWeather(city: string) {
-    return this._http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=90d0b79bd6205d862f8457b4d0c44e76`)
+    return this._http.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=`)
     .map( data => data.json() )
     .toPromise();
   }
